9a0116f01cdfe3056a0848f629dbff1bca2fdef9
Victoria Hale <vic.hale74@gmail.com>
2020-01-18 12:03:58 -0800

    added validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,5 +2,7 @@ class Entry < ApplicationRecord
     belongs_to :user 
     belongs_to :topic
     accepts_nested_attributes_for :topic, reject_if: :all_blank
+    validates :date, presence: :true
+    validates :content, presence: :true
     scope :my_entries, ->{where(topic_id: "topic.id")}
 end
